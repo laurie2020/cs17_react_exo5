28f69ff553e472926d622dfd336b1f1c4f643642
Laurie <laurieiula2016@gmail.com>
2021-03-23 15:13:22 +0100

    exo5

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <ul>
+                    <li>Home</li>
+                    <li>Featured</li>
+                    <li>Contact</li>
+                </ul>
+            </header>
+        )
+    }
+}
